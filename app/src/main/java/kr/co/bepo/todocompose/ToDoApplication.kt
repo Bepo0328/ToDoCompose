d79beb237974192298f31f8554626f9418a6fc56
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-10-08 19:49:57 +0900

    ToDoCompose: Dagger Hilt 셋업

---
+++ app/src/main/java/kr/co/bepo/todocompose/ToDoApplication.kt
@@ -0,0 +1,7 @@
+package kr.co.bepo.todocompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ToDoApplication: Application()
---
+++ app/src/main/java/kr/co/bepo/todocompose/ui/viewmodels/SharedViewModel.kt
@@ -0,0 +1,32 @@
+package kr.co.bepo.todocompose.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kr.co.bepo.todocompose.data.models.ToDoTask
+import kr.co.bepo.todocompose.data.repositories.ToDoRepository
+import javax.inject.Inject
+
+@HiltViewModel
+class SharedViewModel @Inject constructor(
+    private val repository: ToDoRepository
+): ViewModel() {
+
+    private val _allTasks =
+        MutableStateFlow<List<ToDoTask>>(emptyList())
+
+    val allTasks: StateFlow<List<ToDoTask>> = _allTasks
+
+    fun getAllTasks() {
+        viewModelScope.launch {
+            repository.getAllTasks.collect {
+                _allTasks.value = it
+            }
+        }
+    }
+
+}
