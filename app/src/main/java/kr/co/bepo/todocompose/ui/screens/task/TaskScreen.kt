8f5acc08e1c4aa6e96fe7a2b7da32204fe53c0ae
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-10-16 22:58:56 +0900

    ToDoCompose: TaskScreen 구성 - 1
    # NewTaskAppBar 생성

---
+++ app/src/main/java/kr/co/bepo/todocompose/ui/screens/task/TaskScreen.kt
@@ -0,0 +1,14 @@
+package kr.co.bepo.todocompose.ui.screens.task
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TaskScreen() {
+    Scaffold(
+        topBar = {
+
+        },
+        content = {}
+    )
+}
